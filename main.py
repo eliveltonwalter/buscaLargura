dd681bca9cf8d195b3915da5fda2abc516e1e0f9
Elivelton Walter <33106193+eliveltonwalter@users.noreply.github.com>
2020-10-27 23:08:18 -0300

    [feature] need to improve getPath

--- main.py
+++ main.py
@@ -3,63 +3,75 @@ import math
 from collections import deque
 
 C = [ 'C1', 'C2', 'C3', 'C4', 'C5']
-L = [['C4','C2'],['C3','C5'], [], [], []]
+L = [['C4','C2'],['C1','C3','C5'], ['C2'], ['C1'], ['C2']]
 
 def main():
-    adjacentMatrix = generateGraph(C,L)
+    N = generateGraph(C,L)
     source = 'C1'
     destination = 'C5'
-    p = shortestPath(adjacentMatrix, source, destination)
+    p = shortestPath(N, source, destination)
+    print(p)
 
-def shortestPath(adjacentMatrix, source, destination):
-    intSource = int(source[1])
-    intDestination = int(source[1])
-    pathMatrix = deepSearch(adjacentMatrix, intSource)
-    return pathMatrix
+def shortestPath(N, source, destination):
+    intSource = int(source[1])-1
+    intDestination = int(destination[1])-1
+    Dv,Av = deepSearch(N, intSource)
+    path = getPath(intSource,intDestination,Dv,Av)
+    return path
 
-def deepSearch(adjacentMatrix, s):
+def getPath(source,destination,Dv,Av):
+    currentPosition = Av[destination]
+    path = deque([])
+    path.append(destination)
+    while currentPosition != source:
+        path.appendleft(currentPosition)
+        currentPosition = Av[currentPosition]
+    else:
+        path.appendleft(currentPosition)
+    return path
+    
+def deepSearch(N, s):
     #configurando todos os vértices
     Cv = []
     Dv = []
     Av = []
-    for i in range(len(C)-1):
+    for i in range(len(C)):
         Cv.append(False)
         Dv.append(math.inf)
         Av.append(None)
     #configurando o vértice de origem
-    Cv[0] = True
-    Dv[0] = 0
+    Cv[s] = True
+    Dv[s] = 0
     #preparando a fila de visitas
-    C.pop(s)
-    Q = deque(C)
+    Q = deque([])
+    Q.append(s)
     #Q.pop(s)
     #propagação das visitas
     while len(Q) > 0:
         u = Q.popleft()
-        for i, v in enumerate(adjacentMatrix):
-            print(i)
-            print(v)
-        else:
-            break
+        for v in N[u]:
+            if(Cv[v] == False):
+                Cv[v] = True
+                Dv[v] = Dv[u]+1
+                Av[v] = u
+                Q.append(v)
     return Dv,Av
 
 def generateGraph(C,L):
     g = Graph()
     g.add_vertices(len(C))
     matrixGraph = []
-    vectorConections = [None]*len(C)
+    N = [None]*len(C)
     for i, vI in enumerate(C):
+        tempConections = []
         if(len(L[i]) > 0):
-            tempConections = []
             for j, vJ  in L[i]:
                 matrixGraph.append( (i,int(vJ)-1) )
                 tempConections.append(int(vJ)-1)
-        vectorConections[i] = tempConections
-    print(matrixGraph)
-    print(vectorConections)
+        N[i] = tempConections
     g.add_edges(matrixGraph)
     plot(g, vertex_label=C, vertex_color="white")
-    return matrixGraph
+    return N
             
 
 if __name__ == "__main__":
